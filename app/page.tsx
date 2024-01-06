cb390e8ef61d4924c7768cdcff686349252a4d6f
jonas-kgomo <jonaskgmoo@gmail.com>
2024-01-06 10:02:09 +0300

    adding home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 // app/app/page.tsx
-import { redirect } from 'next/navigation';
+import { redirect } from "next/navigation";
 
-import { Metadata } from "next"
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Finetune LLMs in low resource languages",
   description: "Finetune LLMs in low resource languages.",
-}
+};
 
 export default async function HomePage() {
   // Redirect users to /prompts
-  redirect('/info')
+  redirect("/home");
 }
